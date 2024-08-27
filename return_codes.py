b608de53d2e8e5eda972e4e740cec5e48d60a5af
vibradeporte <info@univlearning.com>
2024-08-27 10:10:03 -0500

    Update return_codes.py

--- return_codes.py
+++ return_codes.py
@@ -1,14 +1,20 @@
 SIN_INFORMACION = 452
 SIN_PERMISOS = 453
+SIN_REGISTROS = 454
+SIN_CURSOS = 455
 
 HTTP_MESSAGES = {
 
     SIN_INFORMACION: "No se encontró información sobre el número de identificación ingresado.",
-    SIN_PERMISOS: "El usuario no tiene los permisos necesarios para acceder al caso de uso."
+    SIN_PERMISOS: "El usuario no tiene los permisos necesarios para acceder al caso de uso.",
+    SIN_REGISTROS: "No se encontró ningún cliente con el ID ingresado.",
+    SIN_CURSOS: "No se encontraron cursos con el nombre ingresado."
 }
 
 """
  ## **Códigos retornados:**
         - 452 -> No se encontró información sobre el número de identificación ingresado.
         - 453 -> El usuario no tiene los permisos necesarios para acceder al caso de uso.
+        - 454 -> No se encontró ningún cliente con el ID ingresado.
+        - 455 -> No se encontró ningún curso con el nombre ingresado.
 """
